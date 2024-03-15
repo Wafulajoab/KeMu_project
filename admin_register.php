6d8ec35c80ae95d17d39e0cf844f0707d47a4e58
Wafulajoab <joabfx22@gmail.com>
2024-03-15 14:24:52 +0300

    update

--- admin_register.php
+++ admin_register.php
@@ -12,11 +12,11 @@
             padding: 0;
         }
         .container {
-            max-width: 400px;
+            max-width: 300px;
             margin: 20px auto;
             padding: 20px;
             background-color: #fff;
-            border-radius: 8px;
+            border-radius: 50px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
         h2 {
